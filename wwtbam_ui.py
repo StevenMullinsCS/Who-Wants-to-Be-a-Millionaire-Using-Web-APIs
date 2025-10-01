a39f88f403f9021d5618ae9c3c1b9dcfffe9404f
Steven Mullins <MULLINSSA@ETSU.EDU>
2025-10-01 16:51:22 -0400

    - Began to build UI elements for the game. - Added welcome message, explains how game works and provides options for user to select. Will show at start of game. - Created credits to give proper credit to Sony for the Who Wants To Be A Millionare property, PixelTail for the API usage under licensing. - TODO: Add Winning message, loss message, question representation.

---
+++ wwtbam_ui.py
@@ -0,0 +1,42 @@
+def welcome():
+    print("------------------------------------------------------\n"
+      "|     Welcome to Who Wants to Be a Millionaire!      |\n"
+      "|                                                    |\n"   
+      "|      You will be asked multiple choice questions   |\n"
+      "|     across a variety of subjects. For each correct |\n"
+      "|     answer, you will be awarded a number of points.|\n"
+      "|     As the game continues, the questions will get  |\n"
+      "|     get harder, so be prepared! Can you answer the |\n"
+      "|     Million Dollar Question? Good Luck!            |\n"
+      "|                                                    |\n"
+      "|     1. Play                                        |\n"
+      "|     2. Credits                                     |\n"
+      "|     3. Exit                                        |\n"
+      "------------------------------------------------------\n")
+
+def game_credits():
+    return("------------------------------------------------------\n"
+          "|                                                    |\n"
+          "|         Who Wants To Be a Millionaire?             |\n"
+          "|        Program created by Steven Mullins           |\n"
+          "|                                                    |\n"
+          "|    This application is a personal project          |\n"
+          "|    inspired by the television game show \"Who       |\n"
+          "|    Wants to Be a Millionaire?\". It is not          |\n"
+          "|    affiliated with, endorsed by, or connected      |\n"
+          "|    to Sony Pictures Television or any of its       |\n"
+          "|    affiliated companies. For personal and          |\n"
+          "|    non-commercial use only.                        |\n"
+          "|                                                    |\n"
+          "|    API provided by the fine folks at PixelTail     |\n"
+          "|    Games under the Creative Commons License:       |\n"
+          "|    https://creativecommons.org/licenses/by-sa/4.0/ |\n"
+          "|    https://opentdb.com/                            |\n"
+          "|    https://www.pixeltailgames.com/                 |\n"
+          "|                                                    |\n"
+          "|    Press Enter to return to game menu.             |\n"
+          "|                                                    |\n"
+          "------------------------------------------------------\n")
+
+def win():
+    return
