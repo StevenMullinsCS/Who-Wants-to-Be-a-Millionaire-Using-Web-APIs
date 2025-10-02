cae02e58bfccd270c13b7d4b6b216b2829d72c73
Steven Mullins <MULLINSSA@ETSU.EDU>
2025-10-02 14:43:21 -0400

    - Began building method to create question set from the parsed json file, adding to list of question objects to iterate through.

--- api_handler.py
+++ api_handler.py
@@ -1,17 +1,16 @@
 import requests
 import json
-import questions
-
+from questions import Question
 # URLs for the different datasets used in the project based on difficulty
 url_easy = "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple"
 url_medium = "https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple"
 url_hard = "https://opentdb.com/api.php?amount=5&difficulty=hard&type=multiple"
 
-def parse_question(difficulty):
+def parse_questions(difficulty):
     if difficulty == "EASY":
         qs = requests.get(url_easy)
         qs_json = qs.json()
-        print(qs_json)
+        build_question_set(qs_json)
     elif difficulty == "MED":
         qs = requests.get(url_medium)
 
@@ -22,6 +21,13 @@ def parse_question(difficulty):
 
 
 def build_question_set(set):
-    questions = []
-    for qs in set:
-        questions.insert
+    questions_set = []
+    for q in set:
+        question = Question(
+            qtext = q["question"],
+            diff = q["diff"],
+            ans = q["incorrect_answers"] + q["correct_answer"],
+            correct = q["correct_answer"]
+        )
+        questions_set.insert(question)
+    print(questions_set)
