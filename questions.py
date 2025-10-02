afc0084c438dde222040147835a1499abf4a4253
Steven Mullins <MULLINSSA@ETSU.EDU>
2025-10-02 14:44:12 -0400

    - Renamed "question" in Question to qtext for clarity.

--- questions.py
+++ questions.py
@@ -1,19 +1,19 @@
 import random
-class question():
-    question: str
+class Question:
+    qtext: str
     difficulty: str
     answers: list[str]
     correct_ans: str
 
 
     def __init__(self, q, diff, ans, correct):
-        self.question = q
+        self.qtext = q
         self.diff = diff
         self.ans = random.shuffle(ans)
         self.correct_ans = correct
 
     def __repr__(self):
-        return (f"{question}\n"
+        return (f"{self.qtext}\n"
                 f"\n"
                 f"A. {self.ans[0]}\n"
                 f"B. {self.ans[1]}\n"
