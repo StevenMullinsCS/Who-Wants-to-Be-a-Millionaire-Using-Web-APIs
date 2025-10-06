0fb412a48db0a4bea502167e1191a9ad54283d6d
Steven Mullins <mullinssa@etsu.edu>
2025-10-06 19:23:03 -0400

    - Began modifying the function to set the points for the game - created a list of the possible base game point values. - Modified and fleshed out function to set the difficulty based off points - Created and beganning work on a method to check the answer inputted answer to a question to compare it to the correct answer of the question. - TODO: Finish working on Set_Points. - TODO: Possibly remove get_points. - TODO: Possibly move Difficulty, Points values to Main.

--- game_functionality.py
+++ game_functionality.py
@@ -1,7 +1,18 @@
+from re import match
+from api_handler import parse_questions
 import requests
 import json
 
-points=0
+# Who Wants To Be A Millionaire uses a "Point Ladder" structure. The easiest way I figured out to do the points was to
+# have a stack that pops the values as questions are answered correctly.
+base_point_vals = [100, 200, 300, 500,                     # EASY
+                   1000, 2000, 4000, 8000, 16000,          # MEDIUM
+                   32000, 64000, 125000, 250000, 500000,   # HARD
+                   1000000]                                # WIN CONDITION
+user_points = 0
+difficulty = "EASY"
+
+questions_set = []
 
 def restart():
     uinput = ""
@@ -17,19 +28,39 @@ def restart():
 
 
 def get_points():
-    return points
+    return user_points
 
-def set_points(new_points):
-    points = new_points
+def set_points():
+    i = 0
+    #if difficulty == "ENDLESS":
+      #  user_points = user_points + 100000
+    #else:
+    user_points = base_point_vals.pop()
+    user_points += 1
 
-def set_difficulty():
-    if get_points < 1000:
-        difficulty = "EASY"
-    # Placeholder
-    elif str:
+def set_difficulty(points):
+    if points == 1000:
+        difficulty = "MEDIUM"
+    elif points == 32000:
+        difficulty = "HARD"
+    else:
         return
-def qna():
-    # Print the question for the user
+    return difficulty
 
+def check_answer(question, answer):
+    if answer ==
+def qna():
+    if not questions_set:
+        parse_questions(difficulty)
+    else:
+        q_index = 0
+        print(questions_set[q_index])
+        answer = input("Please select A, B, C, or D.")
+        ans_as_int = answer.char - 64
+        check_answer(questions_set[q_index], answer)
     #Placeholder
     return str
+
+
+
+
