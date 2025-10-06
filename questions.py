357c31e31cc466a9e4e6670ff772a74e61b38e4a
Steven Mullins <MULLINSSA@ETSU.EDU>
2025-10-06 17:10:03 -0400

    - Added comments to better understand what is going on. - Removed shuffle from Question constructor. Shuffle will be done in parsing

--- questions.py
+++ questions.py
@@ -5,13 +5,17 @@ class Question:
     answers: list[str]
     correct_ans: str
 
-
+    # Initialize the Question object using the information from the JSON
     def __init__(self, q, diff, ans, correct):
         self.qtext = q
         self.diff = diff
-        self.ans = random.shuffle(ans)
+        # Questions should not be in the same order as provided by the JSON. If they were, every question would be
+        # correct if answered with A.
+        self.ans = ans
         self.correct_ans = correct
 
+    # Prints the question along with the answers from the list of possible answers. The user will input the corresponding
+    # letter associated with the answer given.
     def __repr__(self):
         return (f"{self.qtext}\n"
                 f"\n"
