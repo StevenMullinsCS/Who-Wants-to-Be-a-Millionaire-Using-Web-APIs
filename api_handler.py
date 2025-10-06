f7673bfbc9ff6b7010f6cba11b07abd6cf01900d
Steven Mullins <MULLINSSA@ETSU.EDU>
2025-10-06 17:08:33 -0400

    - Added a function to check the status code returned by the API call. This allows us to kill the program if the question set is not received. - Modified build_question_set to fix some issues regarding the answers list, the difficulty storage being the incorrect value. - Modified parse_questions to make it so that parse_questions returns the returned value of build_question_set, making it so that we do not need to call two separate methods. - Added comments to explain purpose of the parse_questions function. - TODO: Finish the calls for the medium and hard difficulties. - TODO: Add Comments

--- api_handler.py
+++ api_handler.py
@@ -1,3 +1,5 @@
+from inspect import stack
+
 import requests
 import json
 from questions import Question
@@ -6,28 +8,40 @@ url_easy = "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple"
 url_medium = "https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple"
 url_hard = "https://opentdb.com/api.php?amount=5&difficulty=hard&type=multiple"
 
+def check_response_code(response):
+    if response.status_code == 200:
+        return
+    else:
+        print(f"Error: Could not fetch questions set: Response Code {response.status_code}")
+        exit(1)
+
+# Calls the API depending on the current difficulty based on points. The data from the json will then be fed into the
+# constructor for the question set. Using the API call necessary for the difficulty should help performance in theory,
+# as there is not unnecessary data being stored when the questions are not needed yet.
 def parse_questions(difficulty):
     if difficulty == "EASY":
-        qs = requests.get(url_easy)
-        qs_json = qs.json()
-        build_question_set(qs_json)
+        response = requests.get(url_easy)
+        check_response_code(response)
+        qs_json = response.json()
+        return build_question_set(qs_json["results"])
     elif difficulty == "MED":
         qs = requests.get(url_medium)
-
     else:
         requests.get(url_hard)
 
 
 
 
-def build_question_set(set):
+
+def build_question_set(q_set):
     questions_set = []
-    for q in set:
+    for qs in q_set:
+        all_answers = qs["incorrect_answers"] + [qs["correct_answer"]]
         question = Question(
-            qtext = q["question"],
-            diff = q["diff"],
-            ans = q["incorrect_answers"] + q["correct_answer"],
-            correct = q["correct_answer"]
+            q = qs["question"],
+            diff = qs["difficulty"],
+            ans = all_answers,
+            correct = qs["correct_answer"]
         )
-        questions_set.insert(question)
-    print(questions_set)
+        questions_set.append(question)
+    print(questions_set[0])
