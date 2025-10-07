30d8b2e79bd0c47453f48c06c418f400957b42dc
Steven Mullins <MULLINSSA@ETSU.EDU>
2025-10-07 04:19:35 -0400

    - Began building the main method to be able to play the game properly. - TODO: Improve UI, fix loops to restart or exit program.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from wwtbam_ui import welcome, game_credits
+from game_ui import welcome, game_credits
+import game_functionality as game
 from api_handler import parse_questions, build_question_set
+difficulty = "EASY"
+game.set_points()
+program_active = True
+gameplay = True
 
-parse_questions("EASY")
+while(program_active):
+    welcome()
+    while(gameplay):
+        print("------------------------------------------")
+        game.qna()
+        print("------------------------------------------")
