c19fa7315aa61cc277533500bfcd03f247600105
Steven Mullins <MULLINSSA@ETSU.EDU>
2025-10-14 16:45:39 -0400

    - Added comments, header.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from game_ui import welcome, game_credits
+"""
+main.py is where the game is ran from.
+"""
+from game_ui import welcome
 import game_functionality as game
-from api_handler import parse_questions, build_question_set
+
 difficulty = "EASY"
 game.set_points()
 program_active = True
 gameplay = True
-
-while(program_active):
-    welcome()
-    while(gameplay):
+'''
+Loop to ensure that the program does not just stop after a question or two.
+'''
+while program_active:
+    while gameplay:
         print("------------------------------------------")
+        welcome()
         game.qna()
+        game.restart()
         print("------------------------------------------")
