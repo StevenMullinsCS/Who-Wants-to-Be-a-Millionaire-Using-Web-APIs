49eaee9ba850f0a7b92b1a64d927fb7f953bc3b6
Steven Mullins <MULLINSSA@ETSU.EDU>
2025-10-14 16:45:47 -0400

    - Added comments, header.

--- questions.py
+++ questions.py
@@ -1,3 +1,7 @@
+"""
+questions.py contains the class that allows the questions from the API call to be used in the game. This seemed like
+the best way to do it, as it allows us to call for certain parts easier and store the information easier.
+"""
 import random as rd
 import html
 class Question:
@@ -6,7 +10,9 @@ class Question:
     answers: list[str]
     correct_ans: str
 
-    # Initialize the Question object using the information from the JSON
+    '''
+    Initialize the Question object using the information from the JSON
+    '''
     def __init__(self, q, diff, ans, correct):
         self.qtext = q
         self.diff = diff
@@ -16,8 +22,10 @@ class Question:
         rd.shuffle(self.ans)
         self.correct_ans = correct
 
-    # Prints the question along with the answers from the list of possible answers. The user will input the corresponding
-    # letter associated with the answer given.
+    '''
+    Prints the question along with the answers from the list of possible answers. The user will input the corresponding
+    letter associated with the answer given.
+    '''
     def __repr__(self):
         return (f"{html.unescape(self.qtext)}\n"
                 f"\n"
